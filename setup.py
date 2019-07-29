ee67771e13e33a518db6c37bdac6b6f1afe720a2
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-07-29 22:57:06 +0300

    Add setup script
    Add setup.py and requirement.txt

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+reqs = list()
+with open('requirements.txt') as file:
+    for line in file:
+        line = line.strip()
+
+        if line != '':
+            reqs.append(line)
+
+setup(
+    name='peakdetect',
+    version='1.0',
+    description='Analytic peak finder.',
+    packages=['peakdetect'],
+    url='https://github.com/Anaxilaus/peakdetect',
+    author='Anaxilaus',
+    install_requires=reqs
+)
